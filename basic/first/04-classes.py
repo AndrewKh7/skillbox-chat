eb36d83f126a3b1a3044cad738b91a7c41f91483
Andrey Khrapkov <andrey.i.khrapkov@gmail.com>
2019-12-11 12:41:49 +0300

    upstream

--- basic/first/04-classes.py
+++ basic/first/04-classes.py
@@ -1,17 +1,19 @@
-#  Created by Artem Manchenkov
-#  artyom@manchenkoff.me
-#
-#  Copyright © 2019
-#
-#  Основы ООП, класс, объект, метод и атрибут
-#
 
 class User:
     first_name: str
     last_name: str
 
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name 
 
-john = User()
-john.first_name = "John"
+    def fullName(self):
+        # return " ".join((self.first_name,self.last_name))
+        return f"Full name: {self.first_name} {self.last_name}"
 
-print(john.first_name)
+john = User("John","Doe")
+Artur = User("Artur","Doe")
+
+print(john.fullName())
+print(Artur.fullName())
+
--- basic/second/01-classes.py
+++ basic/second/01-classes.py
@@ -1,7 +1,35 @@
-#  Created by Artem Manchenkov
-#  artyom@manchenkoff.me
-#
-#  Copyright © 2019
-#
-#  Основы ООП - конструктор, наследование, перегрузка, полиморфизм, инкапсуляция
-#
+
+
+class User:
+    first_name: str
+    last_name: str
+
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name 
+
+    def fullName(self):
+        # return " ".join((self.first_name,self.last_name))
+        return f"Full name: {self.first_name} {self.last_name}"
+
+    def show_age(self):
+        print("I have'n age!")
+class AgedUser(User):
+    __age: int
+
+    def __init__(self, first_name, last_name, age):
+        super().__init__(first_name, last_name)
+        self.__age = age
+
+    def fullName(self):
+        return f"{super().fullName()}, {self.__age}"
+
+    def show_age(self):
+        return self.__age
+
+
+aged_jhon = AgedUser("Jhom", "Due", 15)
+artur = User("Artur","Due")
+
+
+print(aged_jhon.fullName())
